592197ca650743c581bb66f18886f0e339be1149
Jeet <madcap.lad@gmail.com>
2016-12-08 01:43:28 +0530

    Closest sum pair & Matrix shift

--- src/ClosestSumElementArray.java
+++ src/ClosestSumElementArray.java
@@ -1,29 +1,38 @@
+import com.sun.deploy.util.SystemUtils;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by Jeet on 06-12-2016.
  */
 public class ClosestSumElementArray {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 8, 2, 5, 6, 7, 10};
+        int[] arr = new int[]{11, 12, 2, 4, 8, 5, 6, 7, 11};
         (new ClosestSumElementArray()).getClosestSum(arr, 50);
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        (new ClosestSumElementArray()).getClosestSum2(arr, 50);
     }
 
     public void getClosestSum(int[] arr, int n) {
         int sum = 0;
-        int minDiff = 2147483647;
+        int minDiff = Integer.MAX_VALUE;
         int f = 0;
         int s = 0;
         int t = 0;
 
-        for(int i = 0; i <= arr.length - 3; ++i) {
-            for(int j = i + 1; j <= arr.length - 2; ++j) {
-                if(arr[i] != arr[j]) {
-                    for(int k = j + 1; k <= arr.length - 1; ++k) {
-                        if(arr[j] != arr[k] && arr[i] != arr[k]) {
+        for (int i = 0; i <= arr.length - 3; i++) {
+            for (int j = i + 1; j <= arr.length - 2; j++) {
+                if (arr[i] != arr[j]) {
+                    for (int k = j + 1; k <= arr.length - 1; k++) {
+                        if (arr[j] != arr[k] && arr[i] != arr[k]) {
                             int add = arr[i] + arr[j] + arr[k];
                             int diff = Math.abs(add - n);
-                            minDiff = minDiff > diff?diff:minDiff;
-                            if(minDiff == diff) {
+                            minDiff = minDiff > diff ? diff : minDiff;
+                            if (minDiff == diff) {
                                 sum = add;
                                 f = i;
                                 s = j;
@@ -35,8 +44,41 @@ public class ClosestSumElementArray {
             }
         }
 
-        System.out.println(String.format("Closest sum- %d", new Object[]{Integer.valueOf(sum)}));
-        System.out.println(String.format("Respective indices-%d,%d,%d", new Object[]{Integer.valueOf(f), Integer.valueOf(s), Integer.valueOf(t)}));
+        System.out.println(String.format("Closest sum- %d", sum));
+        System.out.println(String.format("Respective indices-%d,%d,%d", f, s, t));
+    }
+
+
+    public void getClosestSum2(int[] arr, int n) {
+        int len = arr.length - 1;
+        int sum = 0, minDiff = Integer.MAX_VALUE;
+        int first = 0, second = 0, third = 0;
+        for (int i = 0; i < len - 1; i++) {
+            int j = 0, k = len;
+            while (j < k) {
+                if (j == i) j++;
+                if (arr[i] == arr[j]) j++;
+                if (arr[j] == arr[k] || arr[i] == arr[k]) k--;
+
+                if (i != j && j < k & k != i) {
+
+                    int add = arr[i] + arr[j] + arr[k];
+                    int diff = Math.abs(n - add);
+                    if (diff < minDiff) {
+                        sum = add;
+                        first = i;
+                        second = j;
+                        third = k;
+                    }
+                    minDiff = diff < minDiff ? diff : minDiff;
+                    if (add < n) j++;
+                    else k--;
+                }
+            }
+
+        }
+        System.out.println(String.format("Program2- Closest sum- %d", sum));
+        System.out.println(String.format("Program2- Respective indices-%d,%d,%d", first, second, third));
     }
 
 }
---
+++ src/MatrixShift90Deg.java
@@ -0,0 +1,77 @@
+/**
+ * Created by Jeet on 06-12-2016.
+ */
+public class MatrixShift90Deg {
+
+
+    public static void main(String[] args) {
+        shiftMatrix();
+    }
+
+    public static void shiftMatrix() {
+        int m = 4, n = 4;
+        int[][] matrix = new int[m][n];
+        int k = 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = k++;
+        printMatrix(matrix);
+        printMatrixIn90DShiftedOrder(matrix);
+        shiftMatrix90D(matrix);
+
+
+    }
+
+
+    public static void printMatrixIn90DShiftedOrder(int[][] matrix) {
+        System.out.println("Printing Shifted Matrix: ");
+        for (int i = 0; i <= matrix[0].length - 1; i++) {
+            for (int j = matrix.length - 1; j >= 0; j--) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void printMatrix(int[][] matrix) {
+        System.out.println("Print Method()- Shifted Matrix: ");
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void shiftMatrix90D(int[][] matrix) {
+        int col = matrix[0].length;
+        int row = matrix.length;
+
+        int layers = matrix.length / 2;
+
+
+        for (int i = 0; i < layers; i++) {
+            int topRow = i;
+            int bottomRow = row - 1 - i;
+            int rightCol = col - 1 - i;
+            int leftCol = i;
+            for (int j = 0; j < col-1 - 2 * i; j++) {
+                int temp = matrix[topRow][leftCol+j];
+
+                matrix[topRow][leftCol+j]= matrix[bottomRow-j][leftCol];
+                matrix[bottomRow-j][leftCol]=matrix[bottomRow][rightCol-j];
+
+                matrix[bottomRow][rightCol-j]= matrix[topRow+j][rightCol];
+                matrix[topRow+j][rightCol]= temp;
+
+
+            }
+        }
+        printMatrix(matrix);
+
+    }
+
+
+}
